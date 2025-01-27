82ad6611998078cc1db2ac4198c50a0a05e60a97
thxgoosemah <nahimovzanibek@gmail.com>
2025-01-27 11:16:28 +0500

    update

---
+++ w3school-practice/comments.py
@@ -0,0 +1,10 @@
+# Single-line comment example
+# This is a comment
+print("Welcome to PP2")  # This is another comment
+
+# Multi-line comment example
+"""
+This is a multi-line comment.
+It can span multiple lines.
+"""
+print("Python is very fun!")
---
+++ w3school-practice/data_types.py
@@ -0,0 +1,7 @@
+# Data types in Python
+x = 42        
+y = 3.14      
+z = "Python"   
+print(type(x)) # type of x will be 'int'
+print(type(y)) # type of y will be 'float' 
+print(type(z)) # type of z will be 'string'
