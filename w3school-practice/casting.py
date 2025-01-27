22eaddf9ab17dcfa6e28bdc164b4ba2535f3652b
thxgoosemah <nahimovzanibek@gmail.com>
2025-01-27 11:13:32 +0500

    update

---
+++ w3school-practice/casting.py
@@ -0,0 +1,12 @@
+x = int(1)   # Convert x to a 1
+y = int(2.8) # Convert y to a 2
+z = int("3") # Convert z to a 3
+
+a = float(1)     # Convert a to a 1.0
+b = float(2.8)   # Convert b to a 2.8
+c = float("3")   # Convert c to a 3.0
+d = float("4.2") # Convert d to a 4.2
+
+e = str("s1") # Convert e to a 's1'
+f = str(2)    # Convert f to a '2'
+g = str(3.0)  # Convert g to a '3.0'
