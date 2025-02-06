778fc5b83798aec0c5f3b35b5f2101b29132827d
thxgoosemah <nahimovzanibek@gmail.com>
2025-02-06 07:38:02 +0500

    updated

--- lab04/python/date/03.py
+++ lab04/python/date/03.py
@@ -1,10 +1,4 @@
 from datetime import datetime
-
-# Get the current datetime
 current_datetime = datetime.now()
-
-# Drop the microseconds
 current_datetime_without_microseconds = current_datetime.replace(microsecond=0)
-
-# Print the result
-print(f"Current datetime without microseconds: {current_datetime_without_microseconds}")
+print(f"Current datetime without microseconds: {current_datetime_without_microseconds}") #hello
