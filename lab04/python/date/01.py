12e1fcfeb008e80a72ddad2d1f1c86da676d602c
thxgoosemah <nahimovzanibek@gmail.com>
2025-02-06 07:37:25 +0500

    updated

--- lab04/python/date/01.py
+++ lab04/python/date/01.py
@@ -1,4 +1,4 @@
 from datetime import datetime, timedelta
 current_date = datetime.now()
 five_days_ago = current_date - timedelta(days=5)
-print(f"Five days ago: {five_days_ago.strftime('%Y-%m-%d %H:%M:%S')}")
+print(f"Five days ago: {five_days_ago.strftime('%Y-%m-%d %H:%M:%S')}") 
